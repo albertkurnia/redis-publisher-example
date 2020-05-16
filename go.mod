5ceb0dca775ecff91e389f9d24517095768725d8
albertkurnia <albert.kurniawan@amartha.com>
2020-05-16 15:43:59 +0700

    init project

---
+++ go.mod
@@ -0,0 +1,8 @@
+module redis-publisher-example
+
+go 1.13
+
+require (
+	github.com/go-redis/redis v6.15.7+incompatible // indirect
+	github.com/go-redis/redis/v7 v7.2.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/go-redis/redis"
+)
+
+var (
+	channel = "EXAMPLE"
+)
+
+func main() {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	_, err := client.Ping().Result()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	pubsub := client.Subscribe(channel)
+	_, err = pubsub.Receive()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// PUBLISH
+	counter := 0
+	for counter < 10 {
+		s := rand.NewSource(time.Now().Unix())
+		message := s.Int63()
+
+		_ = client.Publish(channel, message)
+		fmt.Println("Publish: ", message)
+		counter++
+		time.Sleep(time.Second)
+	}
+
+	_ = pubsub.Close()
+	fmt.Println("Pubsub closed. Channel closed.")
+}
